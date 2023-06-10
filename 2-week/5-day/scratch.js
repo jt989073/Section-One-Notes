fd663bb22cf073b53def2c0b39b0d309ad47c11a
James Thompson <thompsonjames0120@gmail.com>
2023-06-10 17:44:59 -0500

    addd solution

--- 2-week/5-day/scratch.js
+++ 2-week/5-day/scratch.js
@@ -1,15 +1,3 @@
-// console.log('hello'[2]);
-// console.log(24 % 6);
-// console.log((false || true) && !true);
-
-// let num = 100;
-// let isBig = num >= 100;
-// let isSmall = num < 100;
-// console.log(isBig || isSmall);
-
-// console.log(1 + 2 * 3);
-// console.log(1 == "1.0");
-
 // butter: $1
 // eggs: $2
 // milk: $3
@@ -102,3 +90,85 @@ if (mostExpensiveGroceries([groceriesA, groceriesD]) === 0) score++;
 if (mostExpensiveGroceries([groceriesA, groceriesD, groceriesC]) === 2) score++;
 
 console.log("You have scored " + score + "/7 points.");
+
+
+
+
+/* 
+check if our operation is capitalize
+	if it is capitalize the first letter
+    return index into first letter capitalize it and concat the rest of the word (slice)
+
+uppercase, 
+	return the word toUpperCase()
+double,
+	return the word + word
+reverse
+	maybe a method
+    create a new string variable
+    loop over the word from the end to the beginning
+		concat each letter to the newString
+    return the word
+*/
+
+
+// function stringChanger(word, operation) {
+//     // Your code here
+//   if(operation === 'capitalize'){
+//   	return word[0].toUpperCase() + word.slice(1)
+//   } else if(operation === 'uppercase'){
+//   	return word.toUpperCase()
+//   } else if(operation === 'double'){
+//   	return word + word
+//   } else if (operation === 'reverse'){
+//   	let newStr = ''
+//   	for(let i = word.length - 1; i >= 0; i--){
+//   	let letter = word[i]
+//   	newStr += letter
+//   	}
+//   	return newStr
+//     // return word.split('').reverse().join('')
+//   } else {
+//   	return word
+//   }
+// }
+
+// // TESTS
+// // DO NOT MODIFY ANYTHING BELOW THIS LINE
+
+// let score = 0;
+
+// if (stringChanger("foo", "capitalize") === "Foo") score++;
+// if (stringChanger("foo", "uppercase") === "FOO") score++;
+// if (stringChanger("foo", "double") === "foofoo") score++;
+// if (stringChanger("foo", "reverse") === "oof") score++;
+
+// if (stringChanger("foo", "unknown") === "foo") score++;
+
+// console.log("You have scored " + score + "/5 points.");
+
+
+
+
+// function isSorted(nums) {
+
+//     // Loop through each number
+//     for (let i = 0 ; i < nums.length ; i++) {
+
+//         // Check if the current number is less than
+//         // the previous number
+//         if (nums[i-1] > nums[i]) {
+//             // If it's not, return false
+//             return false;
+//         }
+//     }
+
+//     // If every number is greater or equal to the previous number
+//     // in the array, it's sorted.
+//     return true;
+
+// }
+
+// console.log(isSorted([1, 2, 3, 4, 5]))  // true
+// console.log(isSorted([2, 2, 4, 4]))  // true
+// console.log(isSorted([1, 2, 4, 3, 5, 6]))  // false
