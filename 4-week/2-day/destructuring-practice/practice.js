8d2d81641fb8035c67aa4cea85d1c9dc124411fd
James Thompson <thompsonjames0120@gmail.com>
2023-06-20 21:12:18 -0500

    added destructuring practice

---
+++ 4-week/2-day/destructuring-practice/practice.js
@@ -0,0 +1,48 @@
+// Basic Destructuring:
+// Given the following object, use destructuring to extract the name and age properties into separate variables.
+let person = { name: 'John', age: 30 };
+
+
+// Default Values:
+// Given the following object, use destructuring to extract the name and country properties. If country is not defined, set it to the default value of 'Unknown'.
+// let user = { name: 'Alice' };
+
+
+
+// Nested Destructuring:
+// Given the following object, use destructuring to extract the title and author properties nested inside the book object.
+// let library = { book: { title: 'JavaScript 101', author: 'Jane Doe' } };
+
+
+// Renaming Variables:
+// Given the following object, use destructuring to extract the first and last properties and rename them to firstName and lastName, respectively.
+// let person = { first: 'John', last: 'Doe' };
+
+
+// Rest Operator:
+// Given the following object, use destructuring with the rest operator to extract the name property into a separate variable and store the rest of the properties in a new object called details.
+// let employee = { name: 'Alice', age: 25, role: 'Developer', department: 'IT' };
+
+
+// Default Values and Renaming:
+// Given the following object, use destructuring to extract the name and city properties, and set default values of 'Unknown' and 'London', respectively.
+// let person = { firstName: 'John', lastName: 'Doe' };
+
+
+
+// Destructuring Function Parameters:
+// Write a function that accepts an object parameter and uses destructuring to extract the name and age properties. Log them to the console.
+
+// Skipping Properties:
+// Given the following object, use destructuring to extract the name property, skip the age property, and extract the country property.
+// let person = { name: 'Alice', age: 30, country: 'USA' };
+
+
+// Destructuring Arrays of Objects:
+// Given the following array of objects, use destructuring to extract the name property of the first and second objects into separate variables.
+// let users = [{ name: 'John' }, { name: 'Alice' }, { name: 'Bob' }];
+
+
+// Nested Destructuring with Default Values:
+// Given the following object, use destructuring to extract the name and age properties from the person object, and also extract the country property from the address object. If country is not defined, set it to the default value of 'Unknown'.
+// let user = { person: { name: 'John', age: 25 }, address: {} };
