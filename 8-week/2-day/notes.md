3fe262c22e58e2cb7b049183fbc37162266c25dc
James Thompson <thompsonjames0120@gmail.com>
2023-07-25 18:14:39 -0500

    added lecture notes

--- 8-week/2-day/notes.md
+++ 8-week/2-day/notes.md
@@ -1,9 +1,14 @@
+## Quizzes + Lecture
+    - Bind Quiz
+    - Call and Apply Quiz
+    - Arrow Functions Review and Context Quiz
+
 ## Bind, Call, Apply
 
 ```js
 class Cat {
   purr() {
-    console.log('meow');
+    console.log("meow");
   }
 
   purrMore() {
@@ -16,27 +21,20 @@ let sayMeow = cat.purrMore;
 sayMeow(); // TypeError: this.purr is not a function
 
 // You can now use the built in Function.bind to ensure your context, `this`,
-// is the cat object
+// is the corret instance of the cat object
 let boundCat = sayMeow.bind(cat);
 
 // You still *need* to invoke the function
 boundCat(); // prints "meow"
 ```
+
 ## Bind vs. Call and Apply
 
-- Apply takes in an array of arguments while call spreads them out in comma-separated values. You can remember this by thinking: a  is for array and c is for comma.
+- Apply takes in an array of arguments while call spreads them out in comma-separated values. You can remember this by thinking: a is for array and c is for comma.
 - Notice that call and apply are essentially interchangeable and follow the same rules as bind with the main difference being that call and apply invoke the function they're being called on, whereas bind returns the function with its context set.
-
 
 ## Arrow functions
 
-- Arrow functions do not have inherent bindings to a this object based on context; instead, their *this* is lexically bound.
+- Arrow functions do not have inherent bindings to a this object based on context; instead, their _this_ is lexically bound.
 
-- This just means that an arrow function's this refers to whatever code contains it, not calls it. This makes them much more flexible for use in method callbacks than normal anonymous functions.
-
-
-## Quizzes + Lecture
-
-- Bind Quiz
-- Call and Apply Quiz
-- Arrow Functions Review and Context Quiz
+- This just means that an arrow function's _this_ refers to whatever code contains it, not calls it. This makes them much more flexible for use in method callbacks than normal anonymous functions.
--- 8-week/2-day/scratch.js
+++ 8-week/2-day/scratch.js
@@ -1,39 +1,37 @@
 // class Cat {
-//     constructor(name) {
-//       this.name = name;
-//     }
-//     sayName() {
-//         console.log(this.name);
-//     }
+//   constructor(name) {
+//     this.name = name;
 //   }
-  
-//   let cat = new Cat("Meowser");
-  
-//   class Dog {
-//     constructor(name) {
-//       this.name = name;
-//     }
+//   sayName() {
+//     console.log(this.name);
 //   }
-  
-//   let dog = new Dog("Fido");
-  
-//   let sayNameFunc = cat.sayName;
-// //   console.log(sayNameFunc()) // throws an error 
-  
-//   let sayHelloCat = sayNameFunc.bind(cat);
-//   console.log(sayHelloCat) // returns the function that is bound 
+// }
+
+// class Dog {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// let cat = new Cat("Meowser");
+// let dog = new Dog("Fido");
+
+// let sayNameFunc = cat.sayName;
+// //   console.log(sayNameFunc()) // throws an error
+
+// let sayHelloCat = sayNameFunc.bind(cat);
+//   console.log(sayHelloCat) // returns the function that is bound
 //   sayHelloCat(); // prints meowser
-  
+
 //   let sayHelloDog = sayNameFunc.bind(dog);
 //   sayHelloDog(); // prints Fido
-
 
 // class Dog {
 //     constructor(name) {
 //       this.name = name;
 //     }
 //   }
-  
+
 //   class Cat {
 //     constructor(name) {
 //         this.name
@@ -46,20 +44,19 @@
 //         return n
 //     }
 //   }
-  
+
 //   let dog = new Dog('Fido');
 //   let cat = new Cat('Meowser');
-  
-//   // make Fido meow 5 times using `call`
+
+  // make Fido meow 5 times using `call`
 //   let purrTimes = cat.purrNTimes
 //   let called = purrTimes.call(dog, 10);
-//   console.log(called) // call evaluates to its return value of the function that is bound 
-  
-//   // make Fido meow 5 times using `apply`
-// let purrTimesApply = cat.purrNTimes
-//   let applied = purrTimesApply.apply(dog, [5]); 
-//   console.log(applied) //  apply evaluates to its return value of the function that is bound 
+//   console.log(called) // call evaluates to its return value of the function that is bound
 
+  // make Fido meow 5 times using `apply`
+// let purrTimesApply = cat.purrNTimes
+//   let applied = purrTimesApply.apply(dog, [5]);
+//   console.log(applied) //  apply evaluates to its return value of the function that is bound
 
 // class Dog {
 //     constructor(name) {
@@ -67,11 +64,12 @@
 //     }
 //     delayedBark() {
 //       setTimeout(function() {
+//         console.log(this)
 //         console.log(this.name);
 //       }, 1000);
 //     }
 //   }
-  
+
 //   let dog = new Dog("Fido");
 //   dog.delayedBark()  // undefined because setTimeout is on the global object and so this in setTimeout is global not what its in
 
@@ -85,7 +83,7 @@
 //       }, 1000);
 //     }
 //   }
-  
+
 //   let dog = new Dog("Fido");
 //   dog.arrowBoundBark();     // "Fido" (after 1 second)
 
@@ -94,3 +92,9 @@
 // }
 
 // SomeFunc()
+
+const height = sizeInInches => `You are ${sizeInInches} inches tall`
+
+const james = height.bind("this value literally doesn't matter at all because arguments/paramenters in JS are ordered", 72)
+
+console.log(james())
