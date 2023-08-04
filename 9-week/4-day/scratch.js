e7376cda00679a6ecdb73f790452256a0ad0b012
James Thompson <thompsonjames0120@gmail.com>
2023-08-03 23:51:42 -0500

    added notes

--- 9-week/4-day/scratch.js
+++ 9-week/4-day/scratch.js
@@ -1,78 +1,61 @@
-const binaryToDecimal = binaryStr => {
-    const rawBinary = binaryStr.slice(2)
-    let reversed = rawBinary.split('').reverse().join('')
-    let sum = 0
-    for(let i = 0; i < reversed.length; i++){
-        const num = Number(reversed[i])
-        // 2 ^ position of the number starting from the right * the number at the position
-        sum += Math.pow(2, i) * num
-    }
-    return sum
-}
+// const binaryToDecimal = binaryStr => {
+//     const numString = binaryStr.slice(2)
+//     const reversed = numString.split('').reverse().join('')
+//     let sum = 0
+//     for(let i = 0; i < reversed.length; i++){
+//         let num = reversed[i]
+//         // 2 ^ position of the number starting from the right * the number at  the position
+//         sum += Math.pow(2, i) * num
+//     }
+//     return sum
+// }
 
-const numString = '0b11001010'
-console.log(binaryToDecimal(numString))
+// const binaryString = '0b11001010'
+// console.log(binaryToDecimal(binaryString))
+// const letterVals = {
+//     A: 10,
+//     B: 11,
+//     C: 12,
+//     D: 13,
+//     E: 14,
+//     F: 15
+// }
 
+// const binaryToDecimal = binaryStr => {
+//     const numString = binaryStr.slice(2)
+//     const reversed = numString.split('').reverse().join('')
+//     let sum = 0
+//     for(let i = 0; i < reversed.length; i++){
 
-const hexLetters = {
-    A: 10,
-    B: 11,
-    C: 12,
-    D: 13,
-    E: 14,
-    F: 15
-}
+//         let char = reversed[i]
+//         if(letterVals[char]){
+//             sum += Math.pow(16, i) * letterVals[char]
+//         } else {
+//             sum += Math.pow(16, i) * char
+//         }
+//     }
+//     return sum
+// }
 
-const hexToDecimal = hexStr => {
-    const rawHex = hexStr.slice(2)
-    const reversed = rawHex.split('').reverse().join('')
-    let sum = 0
-    for(let i = 0; i < reversed.length; i++){
-        const char = reversed[i]
-        
-        if(hexLetters[char.toUpperCase()]){
-            sum += Math.pow(16, i) * hexLetters[char.toUpperCase()]
-            console.log(char.charCodeAt(), 'line 35')
-        } else{
-            sum += Math.pow(16, i) * Number(char)
-        }
-    }
-    return sum
-}
+// const binaryString = '0xF23C'
+// console.log(binaryToDecimal(binaryString))
 
+// console.log(String.fromCharCode(65)) 
+// console.log(String.fromCharCode('0x41')) 
 
-// const hexString = '0xA'
-// const decimal = hexToDecimal(hexString)
-// console.log()
+// const someHex = '0x2B'
+// console.log(String.fromCharCode(someHex))
 
-// // const num = 
+// const str = 'ABC';
+// console.log(str.charCodeAt(0));
+// console.log(str.charCodeAt(1));
 
-// console.log(String.fromCharCode(65))
-
-
-// let binNum = parseInt('10110', 2);      // 22
-// let binStr = Number(binNum).toString(2);   // '10110'
-// console.log(binNum, binStr)
-
-
-
-console.log(String.fromCharCode(65));
-console.log(String.fromCharCode(66))
-console.log(String.fromCharCode(67))
+// Convert Hexadecimal to Base10 & back`
+let hexNum = parseInt('0xa1', 16);      // 161
+console.log(hexNum)
+let hexStr = Number(hexNum).toString(16);  // 'a1'
 
 
-const str = 'ABC';
-console.log(str.charCodeAt(0));
-
-const str1 = '0x41'
-const str2 = '0x42'
-const str3 = '0x43'
-const str4 = '0x44'
-const str5 = '0x45'
-const someDec = hexToDecimal(str1)
-console.log(String.fromCharCode(someDec))
-console.log(String.fromCharCode(str2))
-console.log(String.fromCharCode(str3))
-console.log(String.fromCharCode(str4))
-console.log(String.fromCharCode(str5))
-
+// Convert Binary to Base10 & back`
+let binNum = parseInt('10110', 2);      // 22
+let binStr = Number(binNum).toString(2);   // '10110'
