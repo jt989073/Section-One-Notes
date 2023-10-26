eb1d64d36f46924a3fa4621df90f9b8fc31beeb7
James Thompson <thompsonjames0120@gmail.com>
2023-10-25 20:58:11 -0500

    added solutions

---
+++ 4-Module/21-week/3-day/sequelize-seeders/server/db/seeders/20231026002115-primary-colors.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Color } = require("../models");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await Color.bulkCreate([
+      { name: "red" },
+      { name: "blue" },
+      { name: "yellow" },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkDelete(
+      "Colors",
+      { name: ["red", "blue", "yellow"] },
+      {}
+    );
+  },
+};
