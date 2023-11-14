fe51310ce9ad2760b65f64bffe1441fb655ae515
James <thompsonjames0120@gmail.com>
2023-11-14 16:45:12 -0600

    updated notes

--- 4-Module/23-week/4-day/jwt-exploration/app.js
+++ 4-Module/23-week/4-day/jwt-exploration/app.js
@@ -2,9 +2,9 @@
 require('dotenv').config();
 
 // Import package
-const jwt = require('jsonwebtoken')
 
 // Your code here 
+const jwt = require('jsonwebtoken')
 
 // Define variables - DO NOT MODIFY
 
@@ -12,11 +12,10 @@ const jwt = require('jsonwebtoken')
 let token; // DO NOT MODIFY! Re-assign the token variable below.
 
 // Your code here 
-token = jwt.sign({
-    email: 'james@someEmail.com'
-},
-process.env.SECRET_KEY,
-{expiresIn: '60s'}
+token = jwt.sign(
+    {email: 'james@someEmail.com'},
+    process.env.SECRET_KEY,
+    {expiresIn: '20s'}
 )
 
 // See the JWT in the console - DO NOT MODIFY
@@ -37,7 +36,7 @@ console.log('Payload:', payload);
 let verifiedPayload; // DO NOT MODIFY! Re-assign the verifiedPayload variable below.
 
 // Your code here 
-verifiedPayload = jwt.verify(token, process.env.SECRET_KEY)
+verifiedPayload = jwt.verify(token, "dc1783e61ab05a9fa1b64d892f4b8edab51c159c7091d57feb955ad5ae8ce9191dbe3a50f95086a018654e6f3c7dbffd6215d656d63a2da811843fc746a664b2")
 
 // See the verified payload in the console - DO NOT MODIFY
 console.log('Verified Payload:', verifiedPayload);
