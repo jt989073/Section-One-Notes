7ec8304ecfba48757a1ee91e27c33d3660dc78f3
jt989073 <thompsonjames0120@gmail.com>
2024-01-31 21:06:44 -0600

    added notes

--- 6-Module/1-week/3-day/scratch.py
+++ 6-Module/1-week/3-day/scratch.py
@@ -1,23 +1,23 @@
-# class Dog:
-#     def sound(self):
-#         return "Woof!"
+class Dog:
+    def sound(self):
+        return "Woof!"
 
-# class Cat:
-#     def sound(self):
-#         return "Meow!"
+class Cat:
+    def sound(self):
+        return "Meow!"
 
-# class Duck:
-#     def sound(self):
-#         return "Quack!"
+class Duck:
+    def sound(self):
+        return "Quack!"
 
-# def make_sound(animal):
-#     return animal.sound()
+def make_sound(animal):
+    return animal.sound()
 
-# # Using duck typing in function
-# dog = Dog()
-# cat = Cat()
-# duck = Duck()
+# Using duck typing in function
+dog = Dog()
+cat = Cat()
+duck = Duck()
 
-# print(make_sound(dog))  # Outputs: Woof!
-# print(make_sound(cat))  # Outputs: Meow!
-# print(make_sound(duck))  # Outputs: Quack!
+print(make_sound(dog))  # Outputs: Woof!
+print(make_sound(cat))  # Outputs: Meow!
+print(make_sound(duck))  # Outputs: Quack!
