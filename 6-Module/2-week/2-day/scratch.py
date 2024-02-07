5b8601ddc9bd242ff287b041692b39d39aebde97
jt989073 <thompsonjames0120@gmail.com>
2024-02-06 22:34:10 -0600

    updated notes

---
+++ 6-Module/2-week/2-day/scratch.py
@@ -0,0 +1,69 @@
+# print(my_dict)
+
+# other_dict = dict(first=1, second=2, banana=3) #naming with kwargs
+# print(other_dict)
+
+# tuple_list = [(1, 'a'),(2, 'b'),(3, 'c')] #(key, value)
+# tuple_dict = dict(tuple_list)
+# print(tuple_dict)
+# print(my_dict)
+# print(my_dict.hello)
+# keys = my_dict.keys()
+# keys = [*keys]
+
+# values = my_dict.values()
+# print(values)
+
+
+# items = my_dict.items()
+# items = [*items]
+# print(items)
+
+# dict_copy = {**my_dict}
+# print(dict_copy)
+
+# new_dict = my_dict
+
+# print(new_dict, my_dict)
+# new_dict['james'] = 'sucks'
+
+# print(new_dict, my_dict)
+
+# my_dict = {'hello': 'world'}
+# my_dict['goodbye'] = 'moon'
+# my_dict['james'] = {'a':  {'a': 'b'}, 'b': 123, 'c': 'string'}
+
+# # for key in my_dict:
+# #     print(key)
+
+# # for key in my_dict.keys():
+# #     print(key)
+
+# # for value in my_dict.values():
+# #     print(value)
+
+# for k,v in my_dict.items():
+#     print(k,v)
+
+# my_set = {1,2,3}
+# other_set = {3,4,5}
+
+# print(my_set | other_set)
+# print(my_set.union(other_set))
+
+# my_set.update([1,2,3,4,5,6,7,8,9])
+# print(my_set)
+
+# print(my_set & other_set)
+# print(my_set.intersection(other_set))
+
+# print(my_set - other_set)
+# print(my_set.difference(other_set))
+# print(other_set - my_set)
+
+
+# print(my_set ^ other_set)
+# print(my_set.symmetric_difference(other_set))
+
+# print(type({}))
+# print(type({1,2,3,4}))
