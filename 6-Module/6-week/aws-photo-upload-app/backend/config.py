39341a74a974732937c0dfb5f943cd3ec4e767ab
jt989073 <thompsonjames0120@gmail.com>
2024-03-09 10:02:56 -0600

    added notes

--- 6-Module/6-week/aws-photo-upload-app/backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-        SECRET_KEY = os.environ.get('SECRET_KEY')
-        SQLALCHEMYH_TRACK_MODIFICATIONS = False
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///dev.db'
